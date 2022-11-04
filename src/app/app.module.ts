350ea893268bf50320735723c1cc77b5bdb9ddc3
MohamedElsayed2202 <mohamed.elsayed.1.3.1999@gmail.com>
2022-11-04 23:24:52 +0200

    local copy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {MaterialExampleModule} from '../material.module';
+import {MatNativeDateModule} from'@angular/material/core'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -14,7 +15,9 @@ import { LoginComponent } from './components/login/login.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatNativeDateModule,
+    MaterialExampleModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
